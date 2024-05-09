cceb0582eb99308d3f98a218ae05b16a4f33a528
Hoon9901 <shonn.dev@gmail.com>
2024-05-09 17:14:38 +0900

    feat: response to PING command
    - PING 명령어에 하드코딩으로 Redis protocol 형식에 따른 응답 결과 반환

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,20 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
+    /**
+     * <p>TCP/IP 기반 Redis 서버 엔트리 포인트</p>
+     *
+     */
     public static void main(String[] args) {
-        ServerSocket serverSocket = null;
+        System.out.println("Hoon Redis");
+        System.out.println("listening on port 6379 (tcp)");
+        ServerSocket serverSocket = null; //
         Socket clientSocket = null;
         int port = 6379;
         try {
@@ -14,6 +24,19 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
+            System.out.println(clientSocket.getLocalAddress() + " is connected");
+            try(InputStream inputStream = clientSocket.getInputStream(); OutputStream outputStream = clientSocket.getOutputStream()) {
+                byte[] buffer = new byte[1024];
+                int dataSize = 0;
+                while((dataSize = inputStream.read(buffer)) != -1) {
+                    String message = new String(buffer, 0, dataSize);
+                    System.out.println(message);
+
+                    PrintWriter writer = new PrintWriter(outputStream);
+                    writer.print("+PONG\r\n");
+                    writer.flush();
+                }
+            }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
