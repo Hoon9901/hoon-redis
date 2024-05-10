3beae01dd76611b886a37d2700ac540058d8bc5f
Hoon9901 <shonn.dev@gmail.com>
2024-05-10 20:55:06 +0900

    chore: Move Logger, ClientTask classes package

--- src/main/java/client/ClientTask.java
+++ src/main/java/client/ClientTask.java
@@ -1,3 +1,7 @@
+package client;
+
+import log.Logger;
+
 import java.io.*;
 import java.net.Socket;
 
@@ -14,29 +18,27 @@ public class ClientTask implements Runnable {
         Logger.info(clientSocket + " is connected");
         try (BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
              PrintWriter pw = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
-            loop(br, pw);
+            while (!clientSocket.isClosed()) {
+                Protocol.process(br, pw);
+            }
         } catch (IOException e) {
             Logger.error("IOException: " + e.getMessage());
+        } catch (Exception e) {
+            Logger.error(e.getMessage());
         } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                    Logger.info(clientSocket + " is close");
-                }
-            } catch (IOException e) {
-                Logger.error("IOException: " + e.getMessage());
-            }
+            close();
         }
     }
 
-    private void loop(BufferedReader br, PrintWriter pw) throws IOException {
-        String data;
-        while ((data = br.readLine()) != null) {
-            Logger.info(data);
-            if (data.equalsIgnoreCase("PING")) {
-                pw.print("+PONG\r\n");
-                pw.flush();
+
+    private void close() {
+        try {
+            if (clientSocket != null) {
+                clientSocket.close();
+                Logger.info(clientSocket + " is close");
             }
+        } catch (IOException e) {
+            Logger.error("IOException: " + e.getMessage());
         }
     }
 
