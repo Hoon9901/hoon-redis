1b2b7eccf8d3df616724d675fb3e1d26b46de0ca
Hoon9901 <shonn.dev@gmail.com>
2024-05-24 13:53:29 +0900

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,13 @@ import java.net.Socket;
 
 public class Main {
 
+    static int port = 6379;
     /**
      * <p>TCP/IP 기반 Redis 서버 엔트리 포인트</p>
      */
     public static void main(String[] args) {
+        config(args);
         printLogo();
-        int port = 6379;
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             serverSocket.setReuseAddress(true);
             loop(serverSocket);
@@ -21,9 +22,17 @@ public class Main {
         }
     }
 
+    public static void config(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("--port")) {
+                port = Integer.parseInt(args[i + 1]);
+            }
+        }
+    }
+
     public static void printLogo() {
         Logger.info("Hoon Redis");
-        Logger.info("listening on port 6379 (tcp)");
+        Logger.info("listening on port %s (tcp)".formatted(port));
     }
 
     public static void loop(ServerSocket serverSocket) throws IOException {
